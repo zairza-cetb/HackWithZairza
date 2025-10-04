49f2a470d74319b3af9b960c4eebe101e1015c40
Jatindra2 <saiprasanna2420@gmail.com>
2025-10-04 21:23:20 +0530

    Update Valid Parentheses_Jatindra2.java

--- DSA/Stack&Queues/Valid Parentheses/Valid Parentheses_Jatindra2.java
+++ DSA/Stack&Queues/Valid Parentheses/Valid Parentheses_Jatindra2.java
@@ -1,3 +1,39 @@
+/*
+Problem: Valid Parentheses
+
+Describtion:
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+3. Every close bracket has a corresponding open bracket of the same type.
+
+
+Approach:
+To check if a string with brackets is valid, we use a stack. This is because brackets must be closed in the reverse order they are opened and a stack helps us track that.
+Steps:
+1. Create an empty stack to store opening brackets '(', '{', '['.
+2. Iterate Through the String:
+    - For each character:
+        - If it is an opening bracket ('(', '{', '['), push it onto the stack.
+        - If it is a closing bracket (')', '}', ']'):
+            - Check if the stack is empty. If so, the string is invalid (no corresponding opening bracket).
+            - Pop the top item from the stack.
+            - Check if it matches the correct type of opening bracket:
+                - For example, ) should match (, } should match {, etc.
+                - If not, return false.
+3. Final Validation:
+    - After processing all characters, the stack should be empty if all brackets were correctly matched and closed.
+    - If not, return false.
+
+
+Time and Space Complexity:
+    - Time Complexity: O(n) — each character is processed exactly once.
+    - Space Complexity: O(n) — in the worst case, the stack holds all opening brackets.
+    
+*/
+
+import java.util.*;
 class Solution {
     public boolean isValid(String s) {
         // Created a Stack to store opening brackets
