20e5f1bc712def4a4813b1a3094ce1cc9794cd46
Jatindra2 <saiprasanna2420@gmail.com>
2025-10-06 11:45:33 +0530

    Update Implement_Queue_using_Stacks_Jatindra2.java

--- DSA/Stack&Queues/Implement Queue using Stacks/Implement_Queue_using_Stacks_Jatindra2.java
+++ DSA/Stack&Queues/Implement Queue using Stacks/Implement_Queue_using_Stacks_Jatindra2.java
@@ -1,6 +1,7 @@
 /*
 Problem: Implement Queue using Stacks
 
+
 Description:
   Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
   Implement the MyQueue class:
@@ -9,12 +10,16 @@ Description:
     - int peek() Returns the element at the front of the queue.
     - boolean empty() Returns true if the queue is empty, false otherwise.
 
+
 Approach:
-  Stacks follow LIFO (Last-In-First-Out) order, while queues follow FIFO (First-In-First-Out). To implement a queue using stacks, we need to reverse the order of elements so that the first pushed element is the first to be popped.
-  We can achieve this using two stacks:
+  - Stacks follow LIFO (Last-In-First-Out) order, while queues follow FIFO (First-In-First-Out). To implement a queue using stacks, we need to reverse the order of elements so that 
+    the first pushed element is the first to be popped.
+  - We can achieve this using two stacks:
     - One for input (stack1), where we store elements in the order they arrive.
     - One for output (stack2), from which we retrieve elements in the correct queue order.
-  The move() method transfers elements from stack1 to stack2 only when stack2 is empty. This reverses the order, so the oldest element (first pushed) ends up on top of stack2.
+  - Helper methos move():
+    - The move() method transfers elements from stack1 to stack2 only when stack2 is empty. This reverses the order, so the oldest element (first pushed) ends up on top of stack2.
+  
   Steps for Each Operation:
     1. push(int x)
       - Directly push the element onto stack1.
@@ -27,22 +32,27 @@ Approach:
     4. empty()
       - Return true if both stack1 and stack2 are empty.
 
-Time Complexity:
-  push(x): O(1)   - Direct push to `stack1`.
-  pop(): Average O(1), Worst O(n)   - Transfers elements only if `stack2` is empty. Each element moves at most once from `stack1` to `stack2
-  peek(): Average O(1), Worst O(n)  - Same logic as `pop()`, but doesn't remove the element.
-  empty(): O(1)   - Checks if both stacks are empty.
-Space Complexity: O(n) - at most n elements are stored between the two stacks.
+
+Time & SpaceComplexity:
+  | Operation | Time (Avg) | Time (Worst) | Space |
+  | --------- | ---------- | ------------ | ----- |
+  | `push`    | O(1)       | O(1)         | O(n)  |
+  | `pop`     | O(1)       | O(n)         | O(n)  |
+  | `peek`    | O(1)       | O(n)         | O(n)  |
+  | `empty`   | O(1)       | O(1)         | O(1)  |
+
 
 Example:
-  Operation	stack1	stack2	Output
-  MyQueue q = new MyQueue();
-  q.push(10)	    stack1: [10]	     stack2: []	
-  q.push(20)	    stack1 [10, 20]	   stack2: []	
-  q.peek()	      stack1: []	       stack2: [20, 10]	   return: 10
-  q.pop()	        stack1: []	       stack2: [20]	       return: 10
-  q.pop()	        stack1: []	       stack2: []        return: 20
-  q.empty()	      stack1: []	       stack2: []	         return: true
+  | Operation   | `stack1`   | `stack2`   | Output |
+  | ----------- | ---------- | ---------- | ------ |
+  | `MyQueue()` | `[]`       | `[]`       | —      |
+  | `push(10)`  | `[10]`     | `[]`       | —      |
+  | `push(20)`  | `[10, 20]` | `[]`       | —      |
+  | `peek()`    | `[]`       | `[20, 10]` | `10`   |
+  | `pop()`     | `[]`       | `[20]`     | `10`   |
+  | `pop()`     | `[]`       | `[]`       | `20`   |
+  | `empty()`   | `[]`       | `[]`       | `true` |
+
 
 */
 
